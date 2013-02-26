1e996096567585fcff5a398b2737d5fd2950a7c3
mleczey <mleczey@gmail.com>
2013-02-26 13:53:09 +0100

    Added auto complete draft.

---
+++ bellis-perennis/src/main/java/com/mleczey/swing/autocomplete/AutocompleteField.java
@@ -0,0 +1,23 @@
+package com.mleczey.swing.autocomplete;
+
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+//http://www.java2s.com/Code/Java/Swing-Components/AutocompleteComboBox.htm
+public class AutocompleteField {
+  public static void main(String[] args) {
+    SwingUtilities.invokeLater(new Runnable() {
+      @Override
+      public void run() {
+      }
+    });
+  }
+}
+
+class AutoCompleteFrame extends JFrame {
+  
+}
+
+class JAutoCompleteTextFiled extends JTextField {
+  
+}
